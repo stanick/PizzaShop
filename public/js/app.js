c66b9a4d657a15ffaef52b14249aa8dfbd7dc17b
Andriy Stankevych <job@e-mail.ua>
2016-10-01 19:10:39 +0300

    add js functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,24 @@
-function test(id)
+function add_to_basket(id)
 {
-alert (id);
+var key = 'product_'+id;
+var x = localStorage.getItem(key);
+x = x * 1 + 1;
+localStorage.setItem(key, x);
+alert (x);
 }
+
+function summa()
+{
+var sum = 0;
+for (var i = 0; i < localStorage.length; i++){
+var key = localStorage.key(i);
+if (key.indexOf("product_") == 0) {
+     var x = localStorage.getItem(key);
+     sum = sum + x * 1;
+}
+
+}
+     alert (sum);
+}
+
+
