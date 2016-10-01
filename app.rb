8291481b7d775447fcc17f4b65eb17b0b56ab041
Andriy Stankevych <job@e-mail.ua>
2016-10-01 17:34:10 +0300

    add js

--- app.rb
+++ app.rb
@@ -10,5 +10,6 @@ class Product < ActiveRecord::Base
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+@products = Product.all
+	erb :index
 end
