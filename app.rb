3def8ab66e0c0c7fefcf1de064643a5583273359
Andrey Stankevych <job@e-mail.ua>
2016-10-03 18:03:10 +0300

    finish lesson 35

--- app.rb
+++ app.rb
@@ -9,21 +9,32 @@ set :database, "sqlite3:pizza.db"
 class Product < ActiveRecord::Base
 end
 
+
 get '/' do
 @products = Product.all
 	erb :index
 end
 
 post '/basket' do
-@products = Product.all
-@hh={}
-orders = params[:orders].split(',')
-orders.each do |order|
-item = order.split('-')
-str= item[0]
-str=str.delete 'product_'
-@hh[str]=item[1]
+@orders = parse_orders params[:orders]
+#[["1", "2"], ["2", "1"]]
+@orders.each do |item|
+item[0] = Product.find(item[0])
+end
+  erb :basket
 end
 
-	erb :basket
+def parse_orders orders  
+# "product_1-2,product_2-1,"
+arr2=[]
+orders = orders.split(',') 
+orders.each do |order|
+s1 = order.split('-')
+# [product_1,1]
+s2 = s1[0].split('_')
+# [product,1]
+arr1 = [s2[1],s1[1]]
+arr2 << arr1 
+end
+return arr2
 end
